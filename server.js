73212a9ea303f70f9509633d73c5e44c265e04e0
mikeheiser9 <mikeheiser9@gmail.com>
2018-08-22 20:33:20 -0700

    issue fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static("./app/public"));
 
 // ================================================================================
 // ROUTER
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 
 // require("./routes/apiRoutes")(app);
 require("./app/routes/htmlRoutes")(app);
-require("./app/public")(app);
+// require("./app/public")(app);
 
 // =============================================================================
 // LISTENER
