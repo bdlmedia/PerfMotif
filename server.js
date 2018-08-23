189e4a3c9c13755c0469e7b59eb76933ed9c3823
mikeheiser9 <mikeheiser9@gmail.com>
2018-08-22 17:31:41 -0700

    chanage

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 
 // require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-// require("./public")(app);
+require("./public")(app);
 
 // =============================================================================
 // LISTENER
