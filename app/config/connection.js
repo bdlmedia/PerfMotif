ea4b383c6ba6faeca815bb33cdba67ae960b7326
mikeheiser9 <mikeheiser9@gmail.com>
2018-08-22 17:14:31 -0700

    ttrying to fixissue

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,27 +1,27 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
+// // *********************************************************************************
+// // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// // *********************************************************************************
 
-// Require mysql
-var mysql = require("mysql");
+// // Require mysql
+// var mysql = require("mysql");
 
-// Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: ""
-});
+// // Set up our connection information
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: ""
+// });
 
-// Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Connect to the database
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-// Export connection
-module.exports = connection;
+// // Export connection
+// module.exports = connection;
