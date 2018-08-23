035f448eaa2c548d6d12dd11bbcee9878a432e1c
mikeheiser9 <mikeheiser9@gmail.com>
2018-08-23 12:02:09 -0700

    setting up db connection

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,35 +1,35 @@
-// // *********************************************************************************
-// // api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// // *********************************************************************************
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
 
-// // Dependencies
-// // =============================================================
-// var connection = require("../config/connection.js");
+// Dependencies
+// =============================================================
+var connection = require("../config/connection.js");
 
-// // Routes
-// // =============================================================
-// module.exports = function(app) {
-//   // Get all chirps
-//   app.get("/api/all", function(req, res) {
-//     var dbQuery = "SELECT * FROM chirps";
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+    var dbQuery = "SELECT * FROM website-contact";
 
-//     connection.query(dbQuery, function(err, result) {
-//       if (err) throw err;
-//       res.json(result);
-//     });
-//   });
+    connection.query(dbQuery, function(err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
 
-//   // Add a chirp
-//   app.post("/api/new", function(req, res) {
-//     console.log("Message Sent:");
-//     console.log(req.body);
+  // Add a chirp
+  app.post("/api/new", function(req, res) {
+    console.log("Form Sent:");
+    console.log(req.body);
 
-//     var dbQuery = "INSERT INTO bespoke_db (author, body, created_at) VALUES (?,?,?)";
+    var dbQuery = "INSERT INTO wesbite-contact (name, email, phone, message) VALUES (?,?,?,?)";
 
-//     connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
-//       if (err) throw err;
-//       console.log("Chirp Successfully Saved!");
-//       res.end();
-//     });
-//   });
-// };
+    connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
+      if (err) throw err;
+      console.log("Contact Form Successfully Posted!");
+      res.end();
+    });
+  });
+};
