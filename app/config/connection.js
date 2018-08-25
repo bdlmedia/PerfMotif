40b041a4c25179af3932d74782f7088f425ebc81
mikeheiser9 <mikeheiser9@gmail.com>
2018-08-24 21:09:04 -0700

    trying

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,25 +1,25 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bespoke_db"
-  });
-};
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "bespoke_db"
+//   });
+// };
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+// module.exports = connection;
