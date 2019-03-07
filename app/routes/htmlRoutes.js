1457311cd2eeb9353dc6a7d4828c2c8e0b6e1dda
mikeheiser9 <mikeheiser9@gmail.com>
2019-03-07 13:16:49 -0500

    closing divs and routes

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -15,9 +15,15 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-//   app.get("/tables", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/tables.html"));
-//   });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/under-construction", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/under-construction.html"));
+  });
+
+
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
