3e2e05ac7b442bc63078501b47f1cbb889cb52bd
mikeheiser9 <mikeheiser9@gmail.com>
2019-11-15 17:14:40 -0500

    about to animate painting

--- app/public/js/horizontal.js
+++ app/public/js/horizontal.js
@@ -29,18 +29,28 @@ function MouseWheelHandler(e) {
 }
 });
 
+// SCROLL INDICATOR LOGIC
+
+window.onscroll = function() {scrollAniFunction()};
+
+function scrollAniFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementsByClassName(".bm-menu-nav-item-line span").style.width = scrolled + "%";
+}
+
 // ANIMATIONS
 var headers = document.querySelectorAll("section");
   
 var controller = new ScrollMagic.Controller();
 
-var horizontalSlide = new TimelineMax()
+var horizontalSlide = new TimelineMax();
 // animate panels
-.to("#js-slideContainer", 1,   {x: "-20%", onComplete:addActive,onCompleteParams:[1],onReverseComplete:addActive,onReverseCompleteParams:[0]})    
-.to("#js-slideContainer", 1,   {x: "-40%", onComplete:addActive,onCompleteParams:[2],onReverseComplete:addActive,onReverseCompleteParams:[1]})
-.to("#js-slideContainer", 1,   {x: "-60%", onComplete:addActive,onCompleteParams:[3],onReverseComplete:addActive,onReverseCompleteParams:[2]})
-.to("#js-slideContainer", 1,   {x: "-80%", onComplete:addActive,onCompleteParams:[4],onReverseComplete:addActive,onReverseCompleteParams:[3]})
-
+// .to("#js-slideContainer", 1,   {x: "-20%", onComplete:addActive,onCompleteParams:[1],onReverseComplete:addActive,onReverseCompleteParams:[0]})
+// .to("#js-slideContainer", 1,   {x: "-40%", onComplete:addActive,onCompleteParams:[2],onReverseComplete:addActive,onReverseCompleteParams:[1]})
+// .to("#js-slideContainer", 1,   {x: "-60%", onComplete:addActive,onCompleteParams:[3],onReverseComplete:addActive,onReverseCompleteParams:[2]})
+// .to("#js-slideContainer", 1,   {x: "-80%", onComplete:addActive,onCompleteParams:[4],onReverseComplete:addActive,onReverseCompleteParams:[3]})
 
 // create scene to pin and link animation
 new ScrollMagic.Scene({
@@ -61,3 +71,5 @@ for(i=0;i<headers.length;i++)
 
 //line scroll
 TweenMax.from(".find-out-more-container hr", 2, {width:0}, {width:100}).repeat(1000);
+
+// painting logo
