5edb40671117096566a490dbfe446535dc8a2db7
mikeheiser9 <mikeheiser9@gmail.com>
2019-11-15 13:28:58 -0500

    scrolling working

--- app/public/js/horizontal.js
+++ app/public/js/horizontal.js
@@ -1,3 +1,35 @@
+// ENABLE ONLY VERTICAL SCROLL
+
+$( document ).ready(function() {
+var scroller = {};
+scroller.e = document.getElementById("js-wrapper");
+
+if (scroller.e.addEventListener) {
+    scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
+    scroller.e.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+} else scroller.e.attachEvent("onmousewheel", MouseWheelHandler);
+
+function MouseWheelHandler(e) {
+
+    // cross-browser wheel delta
+    var e = window.event || e;
+    var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
+
+    var pst = $('#js-wrapper').scrollLeft() + delta;
+
+    if (pst < 0) {
+        pst = 0;
+    } else if (pst > $('#js-slideContainer').width()) {
+        pst = $('#js-slideContainer').width();
+    }
+
+    $('#js-wrapper').scrollLeft(pst);
+
+    return false;
+}
+});
+
+// ANIMATIONS
 var headers = document.querySelectorAll("section");
   
 var controller = new ScrollMagic.Controller();
