f0cff1e5dfcd356335e6114327555d0ed767b38c
mikeheiser9 <mikeheiser9@gmail.com>
2019-11-17 03:11:24 -0500

    going to bed

--- app/public/js/horizontal.js
+++ app/public/js/horizontal.js
@@ -41,21 +41,87 @@ TweenMax.from(".find-out-more-container hr", 2, {width:0}, {width:100}).repeat(1
       .from(".txt-4", .3, {opacity: 0, y: 50}) 
       .from(".logo-main", 1, { scaleX: 0, transformOrigin: "left" }, "-=0.5");
     
-// paint animation
+// 
 TweenMax.defaultEase = Power0.easeNone;
 ;
 var controller = new ScrollMagic.Controller({vertical: false});
 var tl = new TimelineMax();
 tl
-.from(".s2-img2-cont", 2, {x: 100})
-.from(".s3-skew", 2, {x: -50});
+.from(".s2-img2-cont", 4, {x: 50})
+.from(".s3-skew", 2, {x: -80})
+// .from(".s4-svg-cont", 4, {x: 100});
 
 new ScrollMagic.Scene({
   triggerElement: "#section02",
   triggerHook: .07,
-  duration: "900"
+  duration: "1000"
 })
   .setTween(tl)
+  .addIndicators({
+    colorTrigger: "#000",
+    colorStart: "#000",
+    colorEnd: "#000",
+  })
+  .addTo(controller);
+
+// portfolio animations
+
+TweenMax.defaultEase = Power0.easeNone;
+;
+var controller = new ScrollMagic.Controller({vertical: false});
+var tl2 = new TimelineMax();
+tl2
+.from(".s4-svg-cont", 4, {x: 100})
+
+new ScrollMagic.Scene({
+  triggerElement: "#section04",
+  triggerHook: .07,
+  duration: "1000"
+})
+  .setTween(tl2)
+  .addIndicators({
+    colorTrigger: "#000",
+    colorStart: "#000",
+    colorEnd: "#000",
+  })
+  .addTo(controller);
+
+// blog animations
+
+TweenMax.defaultEase = Power0.easeNone;
+;
+var controller = new ScrollMagic.Controller({vertical: false});
+var tl3 = new TimelineMax();
+tl3
+.from(".s4-skew", 4, {x: 80});
+
+new ScrollMagic.Scene({
+  triggerElement: "#section05",
+  triggerHook: .05,
+  duration: "300"
+})
+  .setTween(tl3)
+  .addIndicators({
+    colorTrigger: "#000",
+    colorStart: "#000",
+    colorEnd: "#000",
+  })
+  .addTo(controller);
+
+// contact animations
+TweenMax.defaultEase = Power0.easeNone;
+;
+var controller = new ScrollMagic.Controller({vertical: false});
+var tl4 = new TimelineMax();
+tl4
+.from(".s6-skew", 4, {x: 100});
+
+new ScrollMagic.Scene({
+  triggerElement: ".s4-skew",
+  triggerHook: 0.5,
+  duration: "400"
+})
+  .setTween(tl4)
   .addIndicators({
     colorTrigger: "#000",
     colorStart: "#000",
